07117f2382fa5f6ffb338df9c49aa96ec8fb502b
jolly6 <98voonfusing@gmail.com>
2023-10-19 14:51:20 +0800

    Added basic decrypter

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"crypto/cipher"
 	"crypto/rand"
 	"crypto/sha512"
+	"errors"
 	"fmt"
 	"github.com/jollyboss123/passly/pkg/pbkdf2"
 	"github.com/jollyboss123/passly/pkg/pkcs7"
@@ -16,6 +17,7 @@ import (
 
 func main() {
 	masterPass := []byte("password")
+	plain := []byte("plain")
 	log.Println("pw: ", string(masterPass))
 
 	// generate secret key
@@ -25,14 +27,20 @@ func main() {
 	}
 	log.Println("secret: ", string(sk))
 
-	ciphertext, _, err := encrypter(masterPass, sk) // save ciphertext and iv for decryption
+	ciphertext, _, err := encrypter(masterPass, plain, sk) // save ciphertext and iv for decryption
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Printf("ciphertext: %x\n", ciphertext)
+
+	result, err := decrypter(masterPass, ciphertext, sk)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("decrypted: ", string(result))
 }
 
-func encrypter(password, secretKey []byte) (ciphertext []byte, iv []byte, err error) {
+func encrypter(password, plain, secretKey []byte) (ciphertext []byte, iv []byte, err error) {
 	// generate pbkdf2 key from password and secret key
 	key := pbkdf2.Key(password, secretKey, 100_000, 256/8, sha512.New)
 	result := ""
@@ -42,14 +50,14 @@ func encrypter(password, secretKey []byte) (ciphertext []byte, iv []byte, err er
 	log.Println("key: ", result)
 
 	// pad password to aes block size
-	password, err = pkcs7.Pad(password, aes.BlockSize)
+	plain, err = pkcs7.Pad(plain, aes.BlockSize)
 	if err != nil {
 		return nil, nil, err
 	}
-	log.Println("padded: ", string(password))
+	log.Println("padded: ", string(plain))
 
 	// make cipher text
-	ciphertext = make([]byte, aes.BlockSize+len(password))
+	ciphertext = make([]byte, aes.BlockSize+len(plain))
 
 	// make iv and prepend to the cipher text
 	iv = ciphertext[:aes.BlockSize]
@@ -70,12 +78,38 @@ func encrypter(password, secretKey []byte) (ciphertext []byte, iv []byte, err er
 
 	// encrypt with cbc
 	mode := cipher.NewCBCEncrypter(block, iv)
-	mode.CryptBlocks(ciphertext[aes.BlockSize:], password)
+	mode.CryptBlocks(ciphertext[aes.BlockSize:], plain)
 	return ciphertext, iv, nil
 }
 
-func decrypter(ciphertext, secretKey, iv []byte) ([]byte, error) {
-	return nil, nil
+func decrypter(password, ciphertext, secretKey []byte) ([]byte, error) {
+	key := pbkdf2.Key(password, secretKey, 100_000, 256/8, sha512.New)
+	result := ""
+	for _, k := range key {
+		result += fmt.Sprintf("%02X", k)
+	}
+	log.Println("key: ", result)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(ciphertext) < aes.BlockSize {
+		return nil, errors.New("ciphertext too short")
+	}
+	iv := ciphertext[:aes.BlockSize]
+	log.Printf("iv: %x\n", iv)
+
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	if len(ciphertext)%aes.BlockSize != 0 {
+		return nil, errors.New("ciphertext is not a multiple of block size")
+	}
+
+	mode := cipher.NewCBCDecrypter(block, iv)
+	mode.CryptBlocks(ciphertext, ciphertext)
+	return ciphertext, nil
 }
 
 // genSecretKey generates secret key to be store at client side
